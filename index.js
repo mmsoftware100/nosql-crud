a7013fe38166dad8afb57ab8699a14872f948827
Aung Ko Man <aungkoman@gmail.com>
2023-09-05 13:34:58 +0630

    serving public file

--- index.js
+++ index.js
@@ -4,9 +4,15 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
+// serving static files
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
+  res.send('Hello World!')
   res.sendFile(__dirname + '/templates/index.html');
+});
+app.get('/hello', (req, res) => {
+  res.send('Hello World!')
 });
 
 io.on('connection', (socket) => {
